9592c92984dd01cc2909aa61a918bc9c4af42da5
gouarin <loic.gouarin@gmail.com>
2017-12-01 11:16:24 +0100

    fix appveyor

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ cd build
 cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D LIBCLING_BUILD_STATIC=ON ^
       %SRC_DIR%
 
 if errorlevel 1 exit 1
